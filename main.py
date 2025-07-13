7ea6675541be889ee675601d3ebaa197e76e98ab
antho <acal172@wgu.edu>
2025-07-13 14:50:30 -0400

    added test

--- main.py
+++ main.py
@@ -64,11 +64,18 @@ def go(config: DictConfig):
             "max_price": config["etl"]["max_price"],
         },
     )
-        if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+        if "data_check" in steps:
+         _ = mlflow.run(
+        os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+        "main",
+        parameters={
+            "csv": "clean_sample.csv:latest",
+            "ref": "clean_sample.csv:reference",
+            "kl_threshold": config["data_check"]["kl_threshold"],
+            "min_price": config["data_check"]["min_price"],
+            "max_price": config["data_check"]["max_price"]
+        },
+    )
 
         if "data_split" in active_steps:
             ##################
